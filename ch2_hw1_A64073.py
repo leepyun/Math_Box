58a2ad98c54b09d818f33c89c5e066f030d86d8b
leepyun <64685494+leepyun@users.noreply.github.com>
2022-03-14 23:09:24 +0900

    Colaboratory를 통해 생성됨

---
+++ ch2_hw1_A64073.py
@@ -0,0 +1,66 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "ch2_hw1_A64073.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyPnEgfauZnq1UKrPz08iw4u",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/leepyun/Math_Box/blob/main/ch2_hw1_A64073.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 3,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "rQQKLQRCmswT",
+        "outputId": "efdc1452-375f-4b40-f238-b7d0af79aaf1"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Writing sample.py\n"
+          ]
+        }
+      ],
+      "source": [
+        "%%writefile sample.py\n",
+        "print(3)"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        ""
+      ],
+      "metadata": {
+        "id": "DlIzPA5ZnViM"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
